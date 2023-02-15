1ba3ce851ef28fbe8f02858cebcece176f7138c4
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2023-02-15 13:23:33 -0600

    add search & counter

--- todo-figueroa/src/TodoCounter.js
+++ todo-figueroa/src/TodoCounter.js
@@ -1,9 +1,11 @@
-import React from "react";
-import './TodoCounter.css'
-function TodoCounter(){
-    return(
-        <h2 className="TodoCounter"> Has completado 1 de 4 Tareas</h2>
-    )
+
+import React from 'react';
+import './TodoCounter.css';
+
+function TodoCounter({ total, completed }) {
+  return (
+    <h2 className="TodoCounter">Has completado {completed} de {total} TODOs</h2>
+  );
 }
 
-export {TodoCounter};
+export { TodoCounter };
--- todo-figueroa/src/TodoSearch.js
+++ todo-figueroa/src/TodoSearch.js
@@ -1,8 +1,20 @@
-import './TodoSearch.css'
-function TodoSearch(){
-    return(
-        <input  className='TodoSearch' placeholder="Tareas" />
-    );
+import React from 'react';
+import './TodoSearch.css';
+
+function TodoSearch({ searchValue, setSearchValue }) {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="Tareas"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
+  );
 }
 
-export {TodoSearch};
+export { TodoSearch };
