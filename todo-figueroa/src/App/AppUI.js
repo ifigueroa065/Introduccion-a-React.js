03fa7f25c51efdea14517fa2b02ceeec96bff253
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2023-02-23 11:54:40 -0600

    [ADD] - React & Use Context

--- todo-figueroa/src/App/AppUI.js
+++ todo-figueroa/src/App/AppUI.js
@@ -1,36 +1,29 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
-function AppUI({
-  loading,
-  error,
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+  } = React.useContext(TodoContext);
+  
   return (
     <React.Fragment>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
+      <TodoCounter />
+      <TodoSearch />
 
       <TodoList>
         {error && <p>Desespérate, hubo un error...</p>}
         {loading && <p>Estamos cargando, no desesperes...</p>}
-        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer Tarea!</p>}
         
         {searchedTodos.map(todo => (
           <TodoItem
@@ -42,6 +35,7 @@ function AppUI({
           />
         ))}
       </TodoList>
+
 
       <CreateTodoButton />
     </React.Fragment>
--- todo-figueroa/src/TodoCounter/index.js
+++ todo-figueroa/src/TodoCounter/index.js
@@ -1,10 +1,12 @@
-
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
-function TodoCounter({ total, completed }) {
+function TodoCounter() {
+  const { totalTodos, completedTodos } = React.useContext(TodoContext);
+  
   return (
-    <h2 className="TodoCounter">Has completado {completed} de {total} Tareas</h2>
+    <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} Tareas</h2>
   );
 }
 
